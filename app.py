f8ef5c4a22a8bae97f70edc7ac7ca0bd6b6dbcd9
Walter Neto <waan1@aluno.ifal.edu.br>
2026-06-10 12:38:34 -0300

    Fix import statement for requests in app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import os
 from datetime import datetime
-from flask import Flask, render_template, request, jsonify, redirect, url_for, flash
+from flask import Flask, render_template, request, jsonify, redirect, url_for, flash, requests
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user # type: ignore
 from werkzeug.security import generate_password_hash, check_password_hash
 from dotenv import load_dotenv
-import request
 
 app = Flask(__name__)
 
